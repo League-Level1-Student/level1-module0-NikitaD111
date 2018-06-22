d9aa33b50badc631434b41b6ecc127b0740afbdd
NikitaD111 <league@WTS-IM-17.attlocal.net>
2018-06-22 16:47:03 -0700

    CutnessTV/JackIntheBox/SoundEffects(ip)

--- src/CutnessTV.java
+++ src/CutnessTV.java
@@ -5,31 +5,46 @@ import java.net.URI;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class CutnessTV implements ActionListener {
 	JButton button = new JButton();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
 public static void main(String[] args) {
 	new CutnessTV().animals();
 }
 	public void animals(){
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
-		frame.add(button);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button1);
+		panel.add(button2);
 		button.addActionListener(this);
-	
-		
+		button1.addActionListener(this);
+		button2.addActionListener(this);
 	}
 	public void actionPerformed(ActionEvent e) {
 		JOptionPane.showMessageDialog(null, "Button Clicked");
-		
+	if (e.getSource()==button) {
+		  playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+	if (e.getSource()==button1) {
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+	if(e.getSource()==button2) {
+		 playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
 	}
 	void showDucks() {
 	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
 	}
 
 	void showFrog() {
-	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
 	}
 
 	void showFluffyUnicorns() {

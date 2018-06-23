42034a9698ca88c4dfc2b26310a99df98183b179
League Student <league@iMac-6.attlocal.net>
2018-06-22 18:24:11 -0700

    SoundEffects

--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,4 +1,40 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SoundEffects {
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundEffects implements ActionListener {
+	JButton button = new JButton(); 
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+public static void main(String[] args) {
+	new SoundEffects().sounds();
+ }
+	public void sounds() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button1);
+		panel.add(button2);
+		button.addActionListener(this);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Button Clicked");
+		
+	}
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
+	     sound.play();
+	}
 
 }

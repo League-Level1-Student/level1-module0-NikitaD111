758fe5731f13da3082b679268feb3ea954fd99ab
NikitaD111 <league@WTS14.attlocal.net>
2018-07-20 17:20:23 -0700

    Houses

--- src/Houses.java
+++ src/Houses.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	static Robot r = new Robot();
+	static int height = 0;
 
 	public static void main(String[] args) {
 		r.setSpeed(100);
@@ -10,66 +13,65 @@ public class Houses {
 		r.penDown();
 
 		for (int i = 1; i < 11; i++) {
-        
-			house(i*50);
-			//small();
-			//medium();
-			//large();
+			int random = new Random().nextInt(3);
+			if (random == 0) {
+				house("small", 1);
+
+			}
+			if (random == 1) {
+				house("medium", 1);
+
+			}
+			if (random == 2) {
+				house("large", 0);
+
+			}
+
 		}
 	}
 
-	public static void house(String height) {
-		r.setPenColor(0, 45, 255);
-		if(height.equals("small")) {
-		height = 60;
+	public static void house(String size, int pointedroof) {
+		int random1 = new Random().nextInt(255);
+
+		r.setPenColor(random1, random1, random1);
+		if (size.equals("small")) {
+			height = 60;
+
+		} else if (size.equals("medium")) {
+			height = 120;
+
+		} else if (size.equals("large")) {
+			height = 250;
+
 		}
+
 		// create left side
 		r.move(height);
-		r.turn(90);
 		// Create roof
-		r.move(30);
-		r.turn(90);
+		if (pointedroof == 1) {
+			drawPointyRoof();
+		} else if (pointedroof == 0) {
+			drawFlatRoof();
+		}
 		// create right side
 		r.move(height);
 		r.turn(270);
-		r.setPenColor(3, 255, 71);
+		r.setPenColor(13, 216, 82);
 		r.move(30);
 		r.turn(270);
 	}
-	public static void small() {
-		r.setPenColor(255, 3, 233);
-		r.move(60);
-		r.turn(90);
+
+	public static void drawPointyRoof() {
+		r.turn(45);
 		r.move(30);
 		r.turn(90);
-		r.move(60);
-		r.turn(270);
-		r.setPenColor(3, 255, 71);
 		r.move(30);
-		r.turn(270);
+		r.turn(45);
 	}
-	public static void medium() {
-		r.setPenColor(213, 34, 107);
-		r.move(100);
+
+	public static void drawFlatRoof() {
 		r.turn(90);
 		r.move(30);
 		r.turn(90);
-		r.move(100);
-		r.turn(270);
-		r.setPenColor(3, 255, 71);
-		r.move(30);
-		r.turn(270);
-	}
-	public static void large() {
-		r.setPenColor(123, 34, 30);
-		r.move(200);
-		r.turn(90);
-		r.move(30);
-		r.turn(90);
-		r.move(200);
-		r.turn(270);
-		r.setPenColor(3, 255, 71);
-		r.move(30);
-		r.turn(270);
 	}
 }
